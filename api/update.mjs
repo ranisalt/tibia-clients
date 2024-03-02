802b78501b1adcd6bbfa6068bc4a56dec34b6535
Ranieri Althoff <ranisalt@gmail.com>
2024-03-02 20:35:41 +0100

    Save snapshot of download page into archive.org

--- api/update.mjs
+++ api/update.mjs
@@ -9,7 +9,8 @@ const { rest } = new Octokit({ auth: process.env.GITHUB_TOKEN });
  * @returns {Promise<Response>}
  */
 export const GET = async (req) => {
-	const { CRON_SECRET, LAUNCHER_DOWNLOAD_URL } = process.env;
+	const { CRON_SECRET, CLIENT_DOWNLOAD_PAGE_URL, LAUNCHER_DOWNLOAD_URL } =
+		process.env;
 
 	if (req.headers.get("authorization") !== `Bearer ${CRON_SECRET}`) {
 		return new Response(null, { status: 401 });
@@ -22,7 +23,7 @@ export const GET = async (req) => {
 	const { clientVersion, launcherVersion } = await getLatestVersion();
 	const filename = `tibia-x64-v${launcherVersion}.tar.gz`;
 
-	let releaseId;
+	let release_id;
 	try {
 		const { data } = await rest.repos.getReleaseByTag({
 			owner: "ranisalt",
@@ -33,19 +34,27 @@ export const GET = async (req) => {
 		const asset = data.assets.find(({ name }) => name === filename);
 		// if the asset exists, we don't need to do anything
 		if (asset) {
-			return new Response(null, { status: 200 });
+			return new Response(null, { status: 204 });
 		}
 
-		releaseId = data.id;
+		release_id = data.id;
 	} catch {
+		const res = await fetch(
+			`https://web.archive.org/save/${CLIENT_DOWNLOAD_PAGE_URL}`,
+		);
+		if (!res.ok) {
+			throw new Error(`Failed to fetch ${CLIENT_DOWNLOAD_PAGE_URL}`);
+		}
+
 		const { data } = await rest.repos.createRelease({
 			owner: "ranisalt",
 			repo: "tibia-clients",
 			tag_name: `v${launcherVersion}`,
 			name: `${clientVersion}`,
+			body: `This release contains the Tibia client version ${clientVersion} and the launcher version ${launcherVersion}.\n\nA snapshot of the download page is available at ${res.url} for integrity checking.`,
 		});
 
-		releaseId = data.id;
+		release_id = data.id;
 	}
 
 	const res = await fetch(LAUNCHER_DOWNLOAD_URL);
@@ -56,12 +65,12 @@ export const GET = async (req) => {
 	await rest.repos.uploadReleaseAsset({
 		owner: "ranisalt",
 		repo: "tibia-clients",
-		release_id: releaseId,
+		release_id: release_id,
 		headers: { contentType: "application/gzip" },
 		// @ts-ignore
 		data: await res.blob(),
 		name: filename,
 	});
 
-	return new Response(null, { status: 200 });
+	return new Response(null, { status: 201 });
 };
